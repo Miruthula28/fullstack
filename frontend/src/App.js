46e5fdbb98e818dd7193a8cec7c746b60e9a0437
Miruthula28 <miruthula.vt@gmail.com>
2025-06-09 14:04:39 +0530

    Added files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import home from './components/home/Home '
+function App() {
+  return (
+    <div className="App">
+      <home/>
+      <h1> Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ResponsiveAppBar from '../navbar/Navbar '
+
+function home(){
+  return (
+    <div>
+      <ResponsiveAppBar/>
+    </div>
+  )
+}
+export default home
